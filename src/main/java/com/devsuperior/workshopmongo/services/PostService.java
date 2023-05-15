8402bdba2a275edb03c4c53027212c9fbe50a699
Luis <lferreira646@gmail.com>
2023-05-15 17:40:17 -0300

    GET fullsearch

--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.workshopmongo.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,22 +27,9 @@ public class PostService {
 		return repository.searchTitle(text).map(postFound -> new PostDTO(postFound));
 	}
 	
-	/*
-	@Transactional(readOnly = true)
-	public PostDTO findById(String id) {
-		Post post = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		return new PostDTO(post);
-	}
-	
-	public List<PostDTO> findByTitle(String text) {
-		List<PostDTO> result = repository.searchTitle(text).stream().map(x -> new PostDTO(x)).toList();
-		return result;
-	}
-	
-	public List<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
+	public Flux<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
 		maxDate = maxDate.plusSeconds(86400); // 24 * 60 * 60
-		List<PostDTO> result = repository.fullSearch(text, minDate, maxDate).stream().map(x -> new PostDTO(x)).toList();
-		return result;
+		return repository.fullSearch(text, minDate, maxDate).map(postFound -> new PostDTO(postFound));
 	}
-	*/
+	
 }
